f16f9ccad53d04adedd38a90576ccb09fd574325
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-22 20:04:51 -0300

    se determina que boton preiono el usuario para su seleccion, segun esta alternativa se activara Musica, Peliculas o Series

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 const iframe = document.getElementsByTagName("iframe"); //se trae los ifraem donde se mostraran los videos
+const boton = document.querySelector("btn");
+
 
 class Multimedia{
     constructor(url){
-        this.url = url;
-        
+        this.url = url; 
     }
     get url(){
-        return this._url
+        return this._url;
     }
     set url(url){
-        this._url = url
+        this._url = url;
     }
     setInicio(valor) {
-        console.log(`Este método es para realizar un cambio en la URL del video`)
-        return
+        console.log(`Este método es para realizar un cambio en la URL del video`);
     }
 }
 
@@ -23,17 +23,36 @@ class Reproductor extends Multimedia(url, id){
         this._id = id;
     }
     get id(){
-        return this._id
+        return this._id;
     }
     set id(id){
-        this._id = id
+        this._id = id;
     }
     playMultimedia() { 
-        console.log(`funcion`)
+        console.log(`funcion`);
     }
     setInicio() {
         //condicion dada en requerimientos
-        this.id.setAtribute("src", this.url)?start=${tiempo}
+        this.id.setAtribute("src", `${this.url}?start=${tiempo}`);
         
     }
 }
+
+let musica = new Reproductor("https://www.youtube.com/embed/", "musica");
+let pelicula = new Reproductor("https://www.youtube.com/embed/", "peliculas");
+let serie = new Reproductor("https://www.youtube.com/embed/", "series");
+
+boton.addEventListener("click", () => {
+    if (boton.valor == "musica") { 
+        id="musica"
+        musica.playMultimedia();
+    }
+    else if(boton.valor == "peliculas") {
+        id = "peliculas"
+        pelicula.playMultimedia();
+    }
+    else if (boton.valor == "series") {
+        id = "series"
+        serie.playMultimedia();
+    }}
+)
