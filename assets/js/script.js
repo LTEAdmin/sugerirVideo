6f8fc1b573ce5dd6129d1929a55e1b905cd70973
Alejandra Flano <aflanog@latiaencanto.cl>
2024-02-23 01:38:20 -0300

    Se termina de configurar la seleccion de botones para mostrar los videos.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,47 @@
-const boton = document.querySelector("btn");
+const botonSerie = document.getElementById("btnSerie");
+const botonPelicula = document.getElementById("btnPelicula");
+const botonMusica = document.getElementById("btnMusica");
 
-let idBoton = "";
+botonMusica.addEventListener("click", function () {
+  let musica = new Reproductor(
+    "https://www.youtube.com/embed/kxyyKU8rgaU",
+    "musica"
+  );
+  musica.playMultimedia();
+});
+
+botonPelicula.addEventListener("click", function () {
+  let pelicula = new Reproductor(
+    "https://www.youtube.com/embed/25GG9LAUHsA?si=-FsVmFYvu9qXv9I0",
+    "peliculas"
+  );
+  pelicula.playMultimedia();
+});
+
+botonSerie.addEventListener("click", function () {
+  let serie = new Reproductor(
+    "https://www.youtube.com/embed/HhesaQXLuRY",
+    "series"
+    );
+    serie.playMultimedia();
+});
 
 class Multimedia {
-
   constructor(url) {
     this._url = url;
-    };
+  }
   get url() {
     return this._url;
-    };
+  }
   set url(url) {
     this._url = url;
   }
   setInicio() {
     console.log(`Este método es para realizar un cambio en la URL del video`);
   }
-};
+}
 
-class Reproductor extends Multimedia(url, id) {
+class Reproductor extends Multimedia {
   constructor(url, id) {
     super(url);
     this._id = id;
@@ -36,29 +59,7 @@ class Reproductor extends Multimedia(url, id) {
     //condicion dada en requerimientos
     this.id.setAtribute("src", `${this.url}?start=${tiempo}`);
   }
-};
-
-//la idea de trabajar con la lectura del boton es que al hacer click se ejecute la funcion playMultimedia, si se desea agregar nuevas opciones
-//de reproduccion se puede hacer de la misma forma agregando un nuevo if
-
-boton.addEventListener("click", () => {
-    debugger;
-  if (boton.valor == "musica") {
-    idBoton = "musica";
-    let musica = new Reproductor("https://www.youtube.com/embed/kxyyKU8rgaU?si=elzhCyZP3J9qfrz6", idBoton);   //si existe mas de una alternativa se puede realizar una funcion que determine la URL segun seleccion del usuario
-    musica.playMultimedia();
-  } else if (boton.valor == "peliculas") {
-    idBoton = "peliculas";
-    let pelicula = new Reproductor(
-      "https://www.youtube.com/embed/I0_qFoptZ4Y?si=Vm_twXjZGkoVpht2", idBoton );
-    pelicula.playMultimedia();
-  } else if (boton.valor == "series") {
-    idBoton = "series";
-    let serie = new Reproductor(
-      "https://www.youtube.com/embed/fWWrW_VLjws?si=1PVbCteqSx923-YY", idBoton );
-    serie.playMultimedia();
-  }
-});
+}
 
 let modulo = (() => {
   //modulo IIFE
